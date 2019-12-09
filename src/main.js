8ed39e97ecbd2610e06ff612ff6acd2eb24c4e0b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-12-09 16:55:43 +0100

    challenge 3 ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import './style.css';
 
-const body = document.querySelector("body");
-console.log('body: ', body);
-const h1 = document.createElement('h1');
-h1.innerHTML = 'kiki';
-body.appendChild(h1);
+
 console.log('coucou');
