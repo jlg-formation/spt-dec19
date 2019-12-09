9e2175cf32f999e7855fdabbd108c499e1b97b28
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-12-09 15:58:50 +0100

    correction bug 2 times exec

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-"use strict";
+import './style.css';
 
 const body = document.querySelector("body");
+console.log('body: ', body);
 const h1 = document.createElement('h1');
 h1.innerHTML = 'kiki';
 body.appendChild(h1);
+console.log('coucou');
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/main.js",
-    style: "./src/style.css"
-  },
+  entry: "./src/main.js",
   output: {
     filename: "bundle.[hash:4].js",
     path: path.resolve(__dirname, "dist")
@@ -22,13 +19,13 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // all options are optional
-      filename: "[name].[hash:4].css",
+      filename: "style.[hash:4].css",
       chunkFilename: "[id].css",
       ignoreOrder: false // Enable to remove warnings about conflicting order
     })
   ],
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
   },
   module: {
     rules: [
