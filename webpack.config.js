c8c3ec083cb4e0116ab0f06dd28e4f243600082e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-12-09 16:15:01 +0100

    challenge 2 ok

--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,46 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserJSPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = {
-  entry: "./src/main.js",
-  output: {
-    filename: "bundle.[hash:4].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html"
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: "style.[hash:4].css",
-      chunkFilename: "[id].css",
-      ignoreOrder: false // Enable to remove warnings about conflicting order
-    })
-  ],
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {}
-          },
-          "css-loader"
-        ]
-      }
-    ]
-  }
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: "./src/main.js",
+    output: {
+      filename: "bundle.[hash:4].js",
+      path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html"
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // all options are optional
+        filename: "style.[hash:4].css",
+        chunkFilename: "[id].css",
+        ignoreOrder: false // Enable to remove warnings about conflicting order
+      })
+    ],
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
+    devtool: isProd ? 'none' : 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {}
+            },
+            "css-loader"
+          ]
+        }
+      ]
+    }
+  };
 };
+
